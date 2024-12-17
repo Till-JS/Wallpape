0e76a9f5526c44e30aaaf0a65f502ffaf29983fb
Till-JS <101404291+Till-JS@users.noreply.github.com>
2024-12-17 01:01:26 +0100

    Initial Commit

---
+++ Wallpape/WallpapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallpapeApp.swift
+//  Wallpape
+//
+//  Created by Till Schneider on 17.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct WallpapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
